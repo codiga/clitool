acea9f7485d7ee930d6ee84ec9a924637cf56e02
Daniel Strong <daniel@codiga.io>
2022-12-09 15:22:41 -0800

    chore: update get rulesets query

--- codiga/graphql/rosie.py
+++ codiga/graphql/rosie.py
@@ -1,4 +1,5 @@
 import typing
+
 """
 All the GraphQL queries for Rosie
 """
@@ -7,7 +8,38 @@ from codiga.graphql.common import do_graphql_query
 
 
 def get_rulesets(api_token: str, ruleset_names: typing.List[str]):
-    return list(map(lambda x: get_ruleset(api_token, x), ruleset_names))
+    """
+    Get rulesets by their names
+
+    :param api_token: the API token to access the GraphQL API
+    :param ruleset_names: the names of all rulesets to fetch
+    """
+    if not ruleset_names or not api_token:
+        raise ValueError
+
+    ruleset_names_string = "[\"" + ("\", \"".join(ruleset_names)) + "\"]"
+    query = """
+        {
+            ruleSetsForClient(names: """ + ruleset_names_string + """){
+            id
+            name
+            rules(howmany: 10000, skip: 0){
+              id
+              name
+              content
+              language
+              ruleType
+              pattern
+              patternMultiline
+              elementChecked
+            }
+          }
+        }
+    """
+    data = do_graphql_query(api_token, {"query": query})
+    if 'ruleSetsForClient' in data:
+        return data['ruleSetsForClient']
+    return None
 
 
 def get_ruleset(api_token: str, ruleset_name: str):
@@ -21,7 +53,7 @@ def get_ruleset(api_token: str, ruleset_name: str):
         raise ValueError
     query = """
         {
-            ruleSet(name: \""""+ruleset_name+"""\"){
+            ruleSet(name: \"""" + ruleset_name + """\"){
             id
             name
             rules(howmany: 10000, skip: 0){
