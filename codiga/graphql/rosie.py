b1df2e790063393e005814457627d0e724aeef05
Daniel Strong <daniel@codiga.io>
2022-12-09 16:22:48 -0800

    chore: remove unused code

--- codiga/graphql/rosie.py
+++ codiga/graphql/rosie.py
@@ -42,39 +42,6 @@ def get_rulesets(api_token: str, ruleset_names: typing.List[str]):
     return None
 
 
-def get_ruleset(api_token: str, ruleset_name: str):
-    """
-    Get a ruleset by its name
-
-    :param api_token: the API token to access the GraphQL API
-    :param ruleset_name: the name of all ruleset to fetch
-    """
-    if not ruleset_name or not api_token:
-        raise ValueError
-    query = """
-        {
-            ruleSet(name: \"""" + ruleset_name + """\"){
-            id
-            name
-            rules(howmany: 10000, skip: 0){
-              id
-              name
-              content
-              language
-              ruleType
-              pattern
-              patternMultiline
-              elementChecked
-            }
-          }
-        }
-    """
-    data = do_graphql_query(api_token, {"query": query})
-    if 'ruleSet' in data:
-        return data['ruleSet']
-    return None
-
-
 def graphql_get_file_analysis(api_token: str, file_analysis_id: int):
     """
     Get the file analysis object with violations
