c67c28d65d589222181c19596474cc93df006547
Daniel Strong <daniel@codiga.io>
2022-12-12 09:36:50 -0800

    chore: add return test case

--- tests/graphql/test_rosie.py
+++ tests/graphql/test_rosie.py
@@ -30,6 +30,67 @@ class TestRosie(unittest.TestCase):
             graphql_get_rulesets("blo", [])
 
     @patch('codiga.graphql.rosie.do_graphql_query')
+    def test_graphql_get_rulesets_valid_argument(self, do_graphql_query_mock):
+        """
+        Check the value returned when called with valid arguments
+        :return:
+        """
+        fake_data = [
+            {
+                "id": 1,
+                "name": "daniel-ruleset",
+                "rules": [
+                    {
+                        "id": 1,
+                        "name": "rule-1",
+                        "content": "cHJpbnQoIkhlbGxvIFdvcmxkISIp",
+                        "language": "Python",
+                        "ruleType": "Ast",
+                        "pattern": None,
+                        "patternMultiline": None,
+                        "elementChecked": "FunctionCall"
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "name": "real-ruleset",
+                "rules": [
+                    {
+                        "id": 3,
+                        "name": "rule-3",
+                        "content": "cHJpbnQoIkhlbGxvIFdvcmxkISIp",
+                        "language": "Python",
+                        "ruleType": "Ast",
+                        "pattern": None,
+                        "patternMultiline": None,
+                        "elementChecked": "FunctionCall"
+                    }
+                ]
+            }
+        ]
+        do_graphql_query_mock.return_value = {"ruleSetsForClient": fake_data}
+        data = graphql_get_rulesets("api_token", ["daniel-ruleset", "real-ruleset"])
+        do_graphql_query_mock.assert_called_with("api_token", {"query": """
+        {
+            ruleSetsForClient(names: """ + get_rulesets_name_string(["daniel-ruleset", "real-ruleset"]) + """){
+            id
+            name
+            rules(howmany: 10000, skip: 0){
+              id
+              name
+              content
+              language
+              ruleType
+              pattern
+              patternMultiline
+              elementChecked
+            }
+          }
+        }"""})
+        self.assertEqual(fake_data, data)
+
+    @patch('codiga.graphql.rosie.do_graphql_query')
     def test_graphql_get_rulesets_call(self, do_graphql_query_mock):
         """
         Check that we call the right method when arguments are valid
