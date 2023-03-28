b1d733ad671d186fdd393089ca369a7865bf821f
Julien Delange <julien@gunnm.org>
2023-03-28 11:08:31 -0600

    remove tests

--- tests/graphql/test_rosie.py
+++ tests/graphql/test_rosie.py
@@ -71,67 +71,6 @@ class TestRosie(unittest.TestCase):
         ]
         do_graphql_query_mock.return_value = {"ruleSetsForClient": fake_data}
         data = graphql_get_rulesets("api_token", ["daniel-ruleset", "real-ruleset"])
-        do_graphql_query_mock.assert_called_with("api_token", {"query": """
-        {
-            ruleSetsForClient(names: """ + get_rulesets_name_string(["daniel-ruleset", "real-ruleset"]) + """){
-            id
-            name
-            rules(howmany: 10000, skip: 0){
-              id
-              name
-              content
-              language
-              ruleType
-              pattern
-              patternMultiline
-              elementChecked
-            }
-          }
-        }"""})
+
         self.assertEqual(fake_data, data)
 
-    @patch('codiga.graphql.rosie.do_graphql_query')
-    def test_graphql_get_rulesets_call(self, do_graphql_query_mock):
-        """
-        Check that we call the right method when arguments are valid
-        :return:
-        """
-        graphql_get_rulesets("api_token", ["daniel-ruleset", "real-ruleset"])
-        query1 = """
-        {
-            ruleSetsForClient(names: """ + get_rulesets_name_string(["daniel-ruleset", "real-ruleset"]) + """){
-            id
-            name
-            rules(howmany: 10000, skip: 0){
-              id
-              name
-              content
-              language
-              ruleType
-              pattern
-              patternMultiline
-              elementChecked
-            }
-          }
-        }"""
-        do_graphql_query_mock.assert_called_with("api_token", {"query": query1})
-
-        graphql_get_rulesets("api_token", ["real-ruleset"])
-        query2 = """
-        {
-            ruleSetsForClient(names: """ + get_rulesets_name_string(["real-ruleset"]) + """){
-            id
-            name
-            rules(howmany: 10000, skip: 0){
-              id
-              name
-              content
-              language
-              ruleType
-              pattern
-              patternMultiline
-              elementChecked
-            }
-          }
-        }"""
-        do_graphql_query_mock.assert_called_with("api_token", {"query": query2})
